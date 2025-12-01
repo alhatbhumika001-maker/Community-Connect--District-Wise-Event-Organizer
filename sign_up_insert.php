e15fffeeac4a1c18a7f951362101e89c00add54b
Nikita-1797 <patilniki2007@gmail.com>
2025-12-01 22:29:46 +0530

    updated

--- sign_up_insert.php
+++ sign_up_insert.php
@@ -1,31 +1,30 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "community_connect");
 
-$full_name = $_POST['full_name'];
-$username = $_POST['username'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$confirm_password = $_POST['confirm_password'];
-$role = $_POST['role'];
-$district = $_POST['district'];
+ $conn=mysqli_connect("localhost","root","","community_connect");
 
-// Remove user_id from insert as it's auto-increment
-$q = "INSERT INTO users (full_name, username, email, password, confirm_password, role, district) 
-      VALUES ('$full_name', '$username', '$email', '$password', '$confirm_password', '$role', '$district')";
+    $full_name=$_POST['full_name'];
+    $username=$_POST['username'];
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $confirm_password=$_POST['confirm_password'];
+    $role=$_POST['role'];
+    $district=$_POST['district'];
+   
 
-$result = mysqli_query($conn, $q);
 
-if ($result) {
-    echo "<script>
-        alert('Account Created Successfully');
-        window.location.href = 'index.php';
-    </script>";
-} else {
-    $error = mysqli_error($conn);
-    echo "<script>
-        alert('Something went wrong: $error');
-        window.location.href = 'signup.php';
-    </script>";
-    exit();
-}
+    $q="insert into users (user_id,full_name,username,email,password,confirm_password,role,district) values ('NULL','$full_name','$username','$email','$password','$confirm_password','$role','$district')";
+
+
+    $result=mysqli_query($conn,$q);
+
+    if($result)
+    {
+        echo "<script>alert('Account Created Succesfully');window.location='index.php';</script>";
+        
+    }
+    else{
+        echo "<script>alert('Something went wrong');window.location='signup.php';</script>";
+    }
+
+
 ?>
