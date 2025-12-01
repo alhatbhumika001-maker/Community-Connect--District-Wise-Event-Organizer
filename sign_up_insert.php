eefdb917288f43548ed36e6f73bb7b1f5b5e472e
Nikita-1797 <patilniki2007@gmail.com>
2025-12-01 20:17:17 +0530

    updated

--- sign_up_insert.php
+++ sign_up_insert.php
@@ -1,30 +1,24 @@
 <?php
 
- $conn=mysqli_connect("localhost","root","","community_connect");
+$conn = mysqli_connect("localhost", "root", "", "community_connect");
 
-    $full_name=$_POST['full_name'];
-    $username=$_POST['username'];
-    $email=$_POST['email'];
-    $password=$_POST['password'];
-    $confirm_password=$_POST['confirm_password'];
-    $role=$_POST['role'];
-    $district=$_POST['district'];
-   
+$full_name = $_POST['full_name'];
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$confirm_password = $_POST['confirm_password'];
+$role = $_POST['role'];
+$district = $_POST['district'];
 
+// Fix: Remove 'user_id' from INSERT because it's auto_increment
+$q = "INSERT INTO users (full_name, username, email, password, confirm_password, role, district) VALUES ('$full_name', '$username', '$email', '$password', '$confirm_password', '$role', '$district')";
 
-    $q="insert into users(user_id,full_name,username,email,password,confirm_password,role,district) values ('NULL','$full_name','$username','$email','$password','$confirm_password','$role','$district')";
+$result = mysqli_query($conn, $q);
 
-
-    $result=mysqli_query($conn,$q);
-
-    if($result)
-    {
-        echo "<script>alert('Account Created Succesfully');window.location='index.php';</script>";
-        
-    }
-    else{
-        echo "<script>alert('Something went wrong');window.location='signup.php';</script>";
-    }
-
+if ($result) {
+    echo "<script>alert('Account Created Successfully'); window.location='index.php';</script>";
+} else {
+    echo "<script>alert('Something went wrong: " . mysqli_error($conn) . "'); window.location='signup.php';</script>";
+}
 
 ?>
