22983a6f89dc3180ed4a4da57b89608e49f989b4
Nikita-1797 <patilniki2007@gmail.com>
2025-12-02 19:01:17 +0530

    updated

--- login_validation.php
+++ login_validation.php
@@ -8,14 +8,20 @@ $link = mysqli_connect('localhost', 'root', '', 'community_connect');
 $q = "SELECT * FROM users WHERE username='$username' AND password='$password'";
 $result = mysqli_query($link, $q);
 
-if (mysqli_num_rows($result) >= 1) {
-    $data = mysqli_fetch_array($result);
-    $_SESSION['login_user'] = $data;
-    header("Location: index.php");
+// Redirect based on user role
+    if ($data['role'] === 'admin') {
+        header("Location: adminDashboard.php");
+    } 
+    elseif ($data['role'] === 'volunteer') {
+        header("Location: volunteerDashboard.php");
+    } 
+    else {
+        header("Location: index.php"); // default user
+    }
+
     exit();
-} else {
-    // Always show the same error message
-    header("Location: login.php?error=1"); // error=1 set flag=1 or not
+    else {
+    header("Location: login.php?error=1");
     exit();
 }
 ?>
