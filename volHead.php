66266dae6649a130b1ae2f674e8bdd45948d4a7b
Nikita-1797 <patilniki2007@gmail.com>
2025-12-03 18:06:51 +0530

    updated

--- volHead.php
+++ volHead.php
@@ -1,26 +1,19 @@
-    <?php
-        session_start();
+<?php
+session_start();
 
-        $link = mysqli_connect("localhost", "root", "", "community_connect");
+$username = "Guest";
 
-        $username = "Guest";
-
-        if (isset($_SESSION['user_id'])) {
-            $uid = $_SESSION['user_id'];
-            $res = mysqli_query($link, "SELECT username FROM users WHERE user_id='$uid'");
-            if ($row = mysqli_fetch_assoc($res)) {
-                $username = $row['username'];
-            }
-        }
+if (isset($_SESSION['login_user'])) {
+    $username = $_SESSION['login_user']['username'];
+}
 ?>
-
-
 
 <div class="initial">
     <div id="brand" class="d-flex align-items-center">
         <img src="lg.png" class="brand-logo" alt="logo">
         <span class="brand-text">Community Connect</span>
     </div>
+
     <div id="user" class="d-flex align-items-center">
         <span class="user-name">
            Welcome <?php echo $username; ?>
