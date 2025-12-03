d198d3ee5c80b6b70d9a9cfbc4f810eec2b36cfd
Bhumika Alhat <alhatbhumika001@gmail.com>
2025-12-03 13:46:31 +0530

    Update sign_up_insert.php

--- sign_up_insert.php
+++ sign_up_insert.php
@@ -9,6 +9,7 @@ $password = $_POST['password'];
 $confirm_password = $_POST['confirm_password'];
 $role = $_POST['role'];
 $district = $_POST['district'];
+$bio = $_POST['bio'];
 
 // Check if password and confirm password match
 if ($password !== $confirm_password) {
@@ -16,23 +17,9 @@ if ($password !== $confirm_password) {
     exit();
 }
 
-// Check if username already exists
-$user_check = mysqli_query($conn,"SELECT * FROM users WHERE username='$username'");
-if(mysqli_num_rows($user_check) > 0){
-    header("Location: signup.php?username_error=Username already exists");
-    exit();
-}
-
-// Check if email already exists
-$email_check = mysqli_query($conn,"SELECT * FROM users WHERE email='$email'");
-if(mysqli_num_rows($email_check) > 0){
-    header("Location: signup.php?email_error=Email already exists");
-    exit();
-}
-
 // Insert Query
-$q = "INSERT INTO users (user_id, full_name, username, email, password, confirm_password, role, district)
-      VALUES (NULL, '$full_name', '$username', '$email', '$password', '$confirm_password', '$role', '$district')";
+$q = "INSERT INTO users (user_id, full_name, username, email, password, confirm_password, role, district,bio)
+      VALUES (NULL, '$full_name', '$username', '$email', '$password', '$confirm_password', '$role', '$district','$bio')";
 
 $result = mysqli_query($conn, $q);
 
